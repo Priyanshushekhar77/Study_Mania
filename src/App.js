ea04707c385fc1c0870734e55660de527ac445f4
Priyanshushekhar77 <way6shekhar@gmail.com>
2024-05-28 08:20:40 +0530

    viewcoursepage created and Component=>core=>VIEW COURSE FOLDER and their file str created. 7)

--- src/App.js
+++ src/App.js
@@ -5,70 +5,60 @@ import { useDispatch, useSelector } from "react-redux"
 // React Router
 import { Route, Routes, useNavigate } from "react-router-dom"
 
-//COMPONENTS
-
-import  Navbar  from "./components/Common/Navbar";
-import ForgotPassword from "./pages/ForgotPassword";
+// Components
+import Navbar from "./components/Common/Navbar"
 import OpenRoute from "./components/core/Auth/OpenRoute"
 import PrivateRoute from "./components/core/Auth/PrivateRoute"
-//DASHBOARD PAGE COMPONENTS
+import AddCourse from "./components/core/Dashboard/AddCourse"
+import Cart from "./components/core/Dashboard/Cart"
+import EditCourse from "./components/core/Dashboard/EditCourse"
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses"
+// import Instructor from "./components/core/Dashboard/Instructor"
+import MyCourses from "./components/core/Dashboard/MyCourses"
 import MyProfile from "./components/core/Dashboard/MyProfile"
 import Settings from "./components/core/Dashboard/Settings"
-import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
-import Cart from "./components/core/Dashboard/Cart";
-//instructors ka kaam hai
-import MyCourses from "./components/core/Dashboard/MyCourses"
-import AddCourse from "./components/core/Dashboard/AddCourse"
-import EditCourse from "./components/core/Dashboard/EditCourse"
-// import Instructor from "./components/core/Dashboard/Instructor"
-//PAGES
-import  {Home } from "./pages/Home";
+import VideoDetails from "./components/core/ViewCourse/VideoDetails"
+import About from "./pages/About"
+import Catalog from "./pages/Catalog"
+import Contact from "./pages/Contact"
+import CourseDetails from "./pages/CourseDetails"
+import Dashboard from "./pages/Dashboard"
+import Error from "./pages/Error"
+import ForgotPassword from "./pages/ForgotPassword"
+// Pages
+import {Home} from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
-import UpdatePassword from "./pages/UpdatePassword";
-import VerifyEmail from "./pages/VerifyEmail";
-import About from "./pages/About"
-import Contact from "./pages/Contact";
-import Dashboard from "./pages/Dashboard";
-import CourseDetails from "./pages/CourseDetails"
-import Catalog from "./pages/Catalog"
-import Error from "./pages/Dashboard";
-//protected routes
-
-import { ACCOUNT_TYPE } from "./utils/constants";
-
+import UpdatePassword from "./pages/UpdatePassword"
+import VerifyEmail from "./pages/VerifyEmail"
+import ViewCourse from "./pages/ViewCourse"
+import { getUserDetails } from "./services/operations/profileAPI"
+import { ACCOUNT_TYPE } from "./utils/constants"
 
 function App() {
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const { user } = useSelector((state) => state.profile)
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      const token = JSON.parse(localStorage.getItem("token"))
+      dispatch(getUserDetails(token, navigate))
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-
-    
-    
-   <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
-   <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-
-      <Route path="courses/:courseId" element={<CourseDetails />} />
+    <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="courses/:courseId" element={<CourseDetails />} />
         <Route path="catalog/:catalogName" element={<Catalog />} />
-
-    
-  {/* Open Route - for Only Non Logged in User */}
-      <Route
-          path="signup"
-          element={
-            <OpenRoute>
-              <Signup />
-            </OpenRoute>
-          }
-        />
-    <Route
+        {/* Open Route - for Only Non Logged in User */}
+        <Route
           path="login"
           element={
             <OpenRoute>
@@ -76,8 +66,7 @@ function App() {
             </OpenRoute>
           }
         />
-
-<Route
+        <Route
           path="forgot-password"
           element={
             <OpenRoute>
@@ -85,8 +74,7 @@ function App() {
             </OpenRoute>
           }
         />
-
-<Route
+        <Route
           path="update-password/:id"
           element={
             <OpenRoute>
@@ -94,19 +82,24 @@ function App() {
             </OpenRoute>
           }
         />
-
-<Route
-          path="verify-email"
+        <Route
+          path="signup"
           element={
             <OpenRoute>
-              <VerifyEmail/>
+              <Signup />
             </OpenRoute>
           }
         />
-
-
-         {/* Private Route - for Only Logged in User */}
-         <Route
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+        {/* Private Route - for Only Logged in User */}
+        <Route
           element={
             <PrivateRoute>
               <Dashboard />
@@ -115,21 +108,9 @@ function App() {
         >
           {/* Route for all users */}
           <Route path="dashboard/my-profile" element={<MyProfile />} />
-      <Route path="dashboard/Settings" element={<Settings />} />
-
-
-  {/* FOR STUDENTS */}
-      {
-        user?.accountType === ACCOUNT_TYPE.STUDENT && (
-          <>
-          <Route path="dashboard/cart" element={<Cart />} />
-          <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
-
-          </>
-        )
-      }
-      {/* //FOR THE INSTRUCTOR */}
-      {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+          <Route path="dashboard/Settings" element={<Settings />} />
+          {/* Route only for Instructors */}
+          {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
             <>
               {/* <Route path="dashboard/instructor" element={<Instructor />} /> */}
               <Route path="dashboard/my-courses" element={<MyCourses />} />
@@ -140,18 +121,42 @@ function App() {
               />
             </>
           )}
+          {/* Route only for Students */}
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route
+                path="dashboard/enrolled-courses"
+                element={<EnrolledCourses />}
+              />
+              <Route path="/dashboard/cart" element={<Cart />} />
+            </>
+          )}
+          <Route path="dashboard/settings" element={<Settings />} />
+        </Route>
 
-    </Route>
+        {/* For the watching course lectures */}
+        <Route
+          element={
+            <PrivateRoute>
+              <ViewCourse />
+            </PrivateRoute>
+          }
+        >
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route
+                path="view-course/:courseId/section/:sectionId/sub-section/:subSectionId"
+                element={<VideoDetails />}
+              />
+            </>
+          )}
+        </Route>
 
-    
-
-    <Route path="*" element={<Error />} />
-
-
-    </Routes>
-
-   </div>
-  );
+        {/* 404 Page */}
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
