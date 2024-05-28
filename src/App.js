342a00911b51e05327def0a4331091dfb9e0c0ca
Priyanshushekhar77 <way6shekhar@gmail.com>
2024-05-28 12:45:42 +0530

    9)Core=>Dashboard=>instructor.jsx file and InstructorDashboard folder created.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ import AddCourse from "./components/core/Dashboard/AddCourse"
 import Cart from "./components/core/Dashboard/Cart"
 import EditCourse from "./components/core/Dashboard/EditCourse"
 import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses"
-// import Instructor from "./components/core/Dashboard/Instructor"
+//last me karna h
+import Instructor from "./components/core/Dashboard/Instructor"
 import MyCourses from "./components/core/Dashboard/MyCourses"
 import MyProfile from "./components/core/Dashboard/MyProfile"
 import Settings from "./components/core/Dashboard/Settings"
@@ -112,7 +113,7 @@ function App() {
           {/* Route only for Instructors */}
           {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
             <>
-              {/* <Route path="dashboard/instructor" element={<Instructor />} /> */}
+              <Route path="dashboard/instructor" element={<Instructor />} />
               <Route path="dashboard/my-courses" element={<MyCourses />} />
               <Route path="dashboard/add-course" element={<AddCourse />} />
               <Route
